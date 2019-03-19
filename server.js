20269cf0780cd06d57e0b740b6c2197b90ceb74d
Alexander Mark Green <iansntxalxe@gmail.com>
2019-03-19 14:57:46 -0400

    shuffles decks

--- server.js
+++ server.js
@@ -340,7 +340,17 @@ app.get('/api/create-room/', (req, res) => {
           let whiteDeck = game.decks.get('whiteDeck').cards;
           let blackDeck = game.decks.get('blackDeck').cards;
 
+          // https://stackoverflow.com/questions/6274339/how-can-i-shuffle-an-array
+          function shuffle(a) {
+            for (let i = a.length - 1; i > 0; i--) {
+                const j = Math.floor(Math.random() * (i + 1));
+                [a[i], a[j]] = [a[j], a[i]];
+            }
+            return a;
+          }
 
+          shuffle(whiteDeck)
+          shuffle(blackDeck)
 
           //TODO shuffle decks`
 
