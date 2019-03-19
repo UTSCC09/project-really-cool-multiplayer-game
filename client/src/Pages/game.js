20e132cdb30cf4c5d267817b2aff548772ac7cdb
Aaron <aaron-chiu22@hotmail.com>
2019-03-18 23:20:14 -0400

    finished picking, judging, game over phase

--- client/src/Pages/game.js
+++ client/src/Pages/game.js
@@ -26,11 +26,25 @@ const PLAYERHUD_STROKE_COLOR = 'black';
 let waitingState = {
   phase: "waiting",
   public: {
-    cards : []
+    blackCard: "Put your funni Joke here: _____",
+    cardCsar: "You",
+    setting: {winningScore: 5},
+    players: [{username:"Jimbo", score: 1},
+              {username:"Craig", score: 2},
+              {username:"Mike", score: 3},
+              {username:"Dungus", score: 4},
+              {username:"Bongo", score: 1},
+              {username:"Current Csar Man", score: 2},
+              {username:"Yo Moma", score: 3},
+              {username:"You", score: 4}],
+    whiteCards: [],
+    winner: ""
   },
   private: {
-
-  }};
+    cards: ["Peen", "BIG Peen", "CHUNGUS Peen", "I'm in love with my cosplay girlfriend. She's a manchild and better than you.", "Eating Out at Denny's", "Mispelling Words", "Funni Scream"],
+    username: "You",
+    socketId: "78iurf3"
+}};
 let pickingState = {
   phase: "picking",
   public: {
@@ -53,20 +67,65 @@ let pickingState = {
     username: "You",
     socketId: "78iurf3"
 }};
-let judgingState = {phase: "judging"};
-let gameoverState = {phase: "game over"};
+let judgingState = {
+  phase: "judging",
+  public: {
+    blackCard: "Put your funni Joke here: _____",
+    cardCsar: "Current Csar Man",
+    setting: {winningScore: 5},
+    players: [{username:"Jimbo", score: 1},
+              {username:"Craig", score: 2},
+              {username:"Mike", score: 3},
+              {username:"Dungus", score: 4},
+              {username:"Bongo", score: 1},
+              {username:"Current Csar Man", score: 2},
+              {username:"Yo Moma", score: 3},
+              {username:"You", score: 4}],
+    whiteCards: ["Did you know? video games make your weenus big", "Smashing Pumpkins", "M&M is also a candy", "Lobotomy", "Afghanistan", "Grey's Anatomy", "Fortnite Skins", "Drake Like's Them Young"],
+    winner: ""
+  },
+  private: {
+    cards: ["Peen", "BIG Peen", "CHUNGUS Peen", "I'm in love with my cosplay girlfriend. She's a manchild and better than you.", "Eating Out at Denny's", "Mispelling Words", "Funni Scream"],
+    username: "You",
+    socketId: "78iurf3"
+}};
+let gameoverState = {
+  phase: "game over",
+  public: {
+    blackCard: "Put your funni Joke here: _____",
+    cardCsar: "Current Csar Man",
+    setting: {winningScore: 5},
+    players: [{username:"Jimbo", score: 1},
+              {username:"Craig", score: 2},
+              {username:"Mike", score: 3},
+              {username:"Dungus", score: 4},
+              {username:"Bongo", score: 1},
+              {username:"Current Csar Man", score: 2},
+              {username:"Yo Moma", score: 3},
+              {username:"You", score: 4}],
+    whiteCards: ["Did you know? video games make your weenus big", "Smashing Pumpkins", "M&M is also a candy", "Lobotomy", "Afghanistan", "Grey's Anatomy", "Fortnite Skins", "Drake Like's Them Young"],
+    winner: "Jimbo"
+  },
+  private: {
+    cards: ["Peen", "BIG Peen", "CHUNGUS Peen", "I'm in love with my cosplay girlfriend. She's a manchild and better than you.", "Eating Out at Denny's", "Mispelling Words", "Funni Scream"],
+    username: "You",
+    socketId: "78iurf3"
+}};
 let lobbyState = {phase: "lobby"};
 
 
 function drawCard(x, y, cardContent, cardColor, textColor) {
   return () => {
-    console.log('ME TOO BUDDY');
     let paper = window.paper;
     var cardCorner = new paper.Point(x, y);
       
     var card = new paper.Path.Rectangle(cardCorner, CARD_WIDTH, CARD_HEIGHT);
     card.fillColor = cardColor || CARD_DEFAULT_COLOR;
+    // TODO: set these at global vars
     card.strokeColor = 'black';
+    card.shadowColor = 'black';
+    card.shadowBlur = '12';
+    card.shadowOffset = new paper.Point(5,5);
   
     var pointTextLocation = new paper.Point(5,20);
   
@@ -105,35 +164,47 @@ function drawCardRow(x, y, width, cards, cardColor, textColor) {
 
 function drawPlayerHUD(x, y, name, score, isCsar) {
   return () => {
-    let paper = window.paper;
-    // draw playHUD
-    var playerHUDPoint = new paper.Point(x,y);
-    var playerHUD = new paper.Path.Rectangle(playerHUDPoint,PLAYERHUD_WIDTH, PLAYERHUD_HEIGHT);
-    playerHUD.fillColor = PLAYERHUD_FILL_COLOR;
-    playerHUD.strokeColor = PLAYERHUD_STROKE_COLOR;
-    // draw play names
-    var playerNameText = new paper.PointText(playerHUDPoint.add(10,20));
-    playerNameText.content = name;
-    var scoreText = new paper.PointText(playerNameText.point.add(0,20));
-    scoreText.content = score;
-    var isCsarText = new paper.PointText(scoreText.point.add(0,20));
-    if(isCsar) {
-        isCsarText.content = 'Current Csar';
-        isCsarText.fillColor = "blue";
-};
-
+      let paper = window.paper;
+      // draw playHUD
+      var playerHUDPoint = new paper.Point(x,y);
+      var playerHUD = new paper.Path.Rectangle(playerHUDPoint,PLAYERHUD_WIDTH, PLAYERHUD_HEIGHT);
+      playerHUD.fillColor = PLAYERHUD_FILL_COLOR;
+      playerHUD.strokeColor = PLAYERHUD_STROKE_COLOR;
+      // draw play names
+      var playerNameText = new paper.PointText(playerHUDPoint.add(10,20));
+      playerNameText.content = name;
+      var scoreText = new paper.PointText(playerNameText.point.add(0,20));
+      scoreText.content = "score: "+score;
+      var isCsarText = new paper.PointText(scoreText.point.add(0,20));
+      if(isCsar) {
+          isCsarText.content = 'Current Csar';
+          isCsarText.fillColor = "blue";
+    };
   }
+}
+
+function drawWinOverlay(isWinner) {
+  return () => {
+    let paper = window.paper;
+    // draw win text
+    var winText = new paper.PointText(300, paper.view.size.height/2 - 100)
+    winText.content = isWinner?"NICE! YOU DA WINNA!":"YOU LOST LMAOOO000!"
+    winText.fontSize = 80;
+
+  };
 }
 
 function drawPlayerInfo(players, curPlayerName, curCardCsar) {
   return() => {
     let paper = window.paper;
-    let tableHeight = paper.view.size.height - TABLE_PLAYER_SPACE - TABLE_PADDING_Y;
-    let tableWidth = paper.view.size.width - 2*TABLE_PADDING_X;
+    let maxWidth = paper.view.size.width;
+    let maxHeight = paper.view.size.height;
+    let tableHeight = maxHeight - TABLE_PLAYER_SPACE - TABLE_PADDING_Y;
+    let tableWidth = maxWidth - 2*TABLE_PADDING_X;
     let player37_y = TABLE_PADDING_Y + (tableHeight/4) - (PLAYERHUD_HEIGHT/2);
     let player23_x = 0;
     let player28_y = TABLE_PADDING_Y + (tableHeight*3/4) - (PLAYERHUD_HEIGHT/2);
-    let player78_x = paper.view.size.width - PLAYERHUD_WIDTH;
+    let player78_x = maxWidth - PLAYERHUD_WIDTH;
     let player4_x = TABLE_PADDING_X + (tableWidth/4) - (PLAYERHUD_WIDTH/2);
     let player4_y = 0;
     let player5_x = TABLE_PADDING_X + (tableWidth/2) - (PLAYERHUD_WIDTH/2);
@@ -141,8 +212,8 @@ function drawPlayerInfo(players, curPlayerName, curCardCsar) {
     let player6_x = TABLE_PADDING_X + (tableWidth*3/4) - (PLAYERHUD_WIDTH/2);
     let player6_y = 0;
     // TODO
-    let player1_x = paper.view.size.width - HAND_PADDING_X/2 - PLAYERHUD_WIDTH/2;
-    let player1_y = paper.view.size.height - PLAYERHUD_HEIGHT;
+    let player1_x = maxWidth - HAND_PADDING_X/2 - PLAYERHUD_WIDTH/2;
+    let player1_y = maxHeight - PLAYERHUD_HEIGHT;
     let playerPoints = [
       {x: player1_x, y: player1_y},
       {x: player23_x, y: player28_y},
@@ -172,20 +243,31 @@ function renderScreen(gameState) {
   return () => {
     let paper = window.paper;
     paper.project.clear();
-    if (gameState.phase = 'picking') {
+    let maxWidth = paper.view.size.width;
+    let maxHeight = paper.view.size.height;
+    if (gameState.phase == 'picking' || gameState.phase == 'waiting' || gameState.phase == 'judging') {
       // draw table
       drawTable().call();
       // draw playerHUD
       drawPlayerInfo(gameState.public.players, gameState.private.username, gameState.public.cardCsar).call();
       // draw cards in hand
-      let myHandWidth = paper.view.size.width - (2*HAND_PADDING_X);
-      let myHandHeight = paper.view.size.height - CARD_HEIGHT;
+      let myHandWidth = maxWidth - (2*HAND_PADDING_X);
+      let myHandHeight = maxHeight - CARD_HEIGHT;
       drawCardRow(HAND_PADDING_X,myHandHeight,myHandWidth,gameState.private.cards).call();
       // draw black card
       // TODO reposition black card to a permanent spot
       let blackCard_x = TABLE_PADDING_X + 30;
       let blackCard_y = TABLE_PADDING_Y + 30;
       drawCard(blackCard_x, blackCard_y, gameState.public.blackCard, 'black', 'white').call();
+      if (gameState.phase == 'judging') {
+        drawCardRow(0, maxHeight/2 - CARD_HEIGHT/2, maxWidth, gameState.public.whiteCards).call();
+      }
+    } else if (gameState.phase == "game over") {
+      let isWinner = gameState.public.winner == gameState.private.username;
+      // draw table
+      drawTable().call();
+      // draw win screen
+      drawWinOverlay(isWinner).call();
     }
   };
 }
@@ -233,10 +315,10 @@ class Game extends React.Component {
     return(
       <div className="game">
         <canvas id="gameCanvas"></canvas>
-        {/* <button onClick={renderScreen(waitingState)}>waiting</button> */}
+        <button onClick={renderScreen(waitingState)}>waiting</button>
         <button onClick={renderScreen(pickingState)}>picking</button>
-        {/* <button onClick={renderScreen(judgingState)}>judging</button> */}
-        {/* <button onClick={renderScreen(gameoverState)}>game over</button> */}
+        <button onClick={renderScreen(judgingState)}>judging</button>
+        <button onClick={renderScreen(gameoverState)}>game over</button>
         {/* <button onClick={renderScreen(lobbyState)}>lobby</button> */}
       </div>
     )
