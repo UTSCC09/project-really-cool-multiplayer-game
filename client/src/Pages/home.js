c81a7d8a1cdf6ac3f3fe84396bd91b7b79ea37d5
BilalKhan <bm.khan@mail.utoronto.ca>
2019-03-26 19:53:45 -0400

    Added log in button to home screen

--- client/src/Pages/home.js
+++ client/src/Pages/home.js
@@ -19,18 +19,28 @@ class Home extends React.Component {
 
   render() {
     return(
-      <div id="main-container" className="d-flex flex-column justify-content-center align-items-center p-2">
-          <h1> Shuffle With Friends </h1>
-          <span>
-            Nickname:
-            <input id="nickname" className="ml-2"></input>
-          </span>
-          If you don't choose one we'll make one for you
-          <span>
-            <button type="button" className="btn btn-primary m-3" onClick={this.createGame}>Host Game</button>
-            <button type="button" className="btn btn-primary m-3">Join a Game</button>
-          </span>
-      </div>
+        <div id="main-container" className="d-flex justify-content-between">
+
+          <div className="d-flex flex-column justify-content-start align-items-start p-2">
+            <button type="button" className="btn btn-primary m-1"> Log in</button>
+          </div>
+
+          <div className="d-flex flex-column justify-content-center align-items-center p-2">
+            <h1> Shuffle With Friends </h1>
+            <span>
+              Nickname:
+              <input id="nickname" className="ml-2"></input>
+            </span>
+            If you don't choose one we'll make one for you
+            <span>
+              <button type="button" className="btn btn-primary m-3" onClick={this.createGame}>Host Game</button>
+              <button type="button" className="btn btn-primary m-3">Join a Game</button>
+            </span>
+          </div>
+          <div className="d-flex flex-column justify-content-end align-items-start p-2 invisible">
+            <button type="button" className="btn btn-primary m-1"> Log in</button>
+          </div>
+        </div>
     )
   }
 }
