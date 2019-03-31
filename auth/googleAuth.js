cade4122a98ce144fe0cd230aaeed35436addcfb
Aaron <aaron-chiu22@hotmail.com>
2019-03-31 18:17:53 -0400

    fixed google auth callbackurl

--- auth/googleAuth.js
+++ auth/googleAuth.js
@@ -9,7 +9,7 @@ module.exports = (passport) => {
     passport.use(new GoogleStrategy({
             clientID: "493612790648-j862qtgn7uua2g4v0v61q0s8j8n6093t.apps.googleusercontent.com",
             clientSecret: "2JOBeVqUqFzIw9LEmgZK_Nup",
-            callbackURL: "http://localhost:5000/auth/google/callback"
+            callbackURL: (process.env.MONGODB_URI+"auth/google/callback" || "http://localhost:5000/auth/google/callback")
         },
         (token, refreshToken, profile, done) => {
             return done(null, {
